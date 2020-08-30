62def682b74c7e105782d3971bbc47ecc457573a
sagardas1 <sagardas.195@gmail.com>
2020-08-30 16:27:48 +0530

    chnages

---
+++ MicroService1/src/main/java/com/microService1/dto/BulkUserDetails.java
@@ -0,0 +1,17 @@
+package com.microService1.dto;
+
+import java.util.List;
+
+public class BulkUserDetails {
+	private List<UserDetails> bulk;
+
+	public List<UserDetails> getBulk() {
+		return bulk;
+	}
+
+	public void setBulk(List<UserDetails> bulk) {
+		this.bulk = bulk;
+	}
+	
+
+}
