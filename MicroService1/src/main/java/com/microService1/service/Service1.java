30f3bbff364e13e486490dfb8d76cbea61f47503
sagardas1 <sagardas.195@gmail.com>
2020-09-01 13:00:36 +0530

    changes

--- MicroService1/src/main/java/com/microService1/service/Service1.java
+++ MicroService1/src/main/java/com/microService1/service/Service1.java
@@ -9,23 +9,21 @@ import com.microService1.dto.UserDetails;
 
 @Service
 public class Service1 {
-	
+
 	@LoadBalanced
 	@Autowired
 	RestTemplate getRestTemplate;
 
 	public String getName(UserDetails userDetails) {
-		int i=1;
+		int i = 1;
 		System.out.println("m1 service");
-		if(i==0) {
+		if (i == 0) {
 			return null;
 		}
-		String g= (String)getRestTemplate.getForObject("http://localhost:8080/test/m2/test", String.class);
+		String g = (String) getRestTemplate.getForObject("http://localhost:8080/test/m2/test", String.class);
 		System.out.println("hgdsvfhgshfvsghavgshav");
 		return g;
-	
+
 	}
 
 }
-
-
