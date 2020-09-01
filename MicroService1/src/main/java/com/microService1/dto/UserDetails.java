538c775b07e866078c0c45a8d79ca260f2a2517c
sagardas1 <sagardas.195@gmail.com>
2020-09-01 12:59:26 +0530

    chnages

--- MicroService1/src/main/java/com/microService1/dto/UserDetails.java
+++ MicroService1/src/main/java/com/microService1/dto/UserDetails.java
@@ -1,21 +1,24 @@
 package com.microService1.dto;
 
 public class UserDetails {
-	
+
 	private String name;
 	private int redgNo;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getRedgNo() {
 		return redgNo;
 	}
+
 	public void setRedgNo(int redgNo) {
 		this.redgNo = redgNo;
 	}
-	
 
 }
