63ed23352c75fdde1fe1e682705c206d521c6f80
sagardas1 <sagardas.195@gmail.com>
2020-09-01 12:59:43 +0530

    changes

--- MicroService1/src/main/java/com/microService1/service/ReservationConfiguration.java
+++ MicroService1/src/main/java/com/microService1/service/ReservationConfiguration.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
 import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.PingUrl;
-import com.netflix.loadbalancer.AvailabilityFilteringRule;
 
 public class ReservationConfiguration {
 
