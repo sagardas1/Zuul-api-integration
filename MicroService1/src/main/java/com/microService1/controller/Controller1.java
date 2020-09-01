e94d41eeb83e60cbec619db66ceff25247e0d209
sagardas1 <sagardas.195@gmail.com>
2020-09-01 13:04:03 +0530

    changes

--- MicroService1/src/main/java/com/microService1/controller/Controller1.java
+++ MicroService1/src/main/java/com/microService1/controller/Controller1.java
@@ -1,15 +1,12 @@
 package com.microService1.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.microService1.dto.UserDetails;
-import com.microService1.service.ReservationConfiguration;
 import com.microService1.service.Service1;
 
 @RestController
