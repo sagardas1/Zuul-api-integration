01f231aa3253ebe3cd3af5f74c0ad18df8c8134a
sagardas1 <sagardas.195@gmail.com>
2020-10-02 22:40:10 +0530

    changes

--- Zull-api-gateway/src/main/java/com/apigateway/ZullApiGatewayApplication.java
+++ Zull-api-gateway/src/main/java/com/apigateway/ZullApiGatewayApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZullApiGatewayApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(ZullApiGatewayApplication.class, args);
 	}
 
