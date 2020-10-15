69475e61a262a0be5f43a4ac3e8fce649f2f9354
sagardas1 <sagardas.195@gmail.com>
2020-10-15 18:35:10 +0530

    changes

--- MicroService2/src/main/java/com/microService2/MicroService2Application.java
+++ MicroService2/src/main/java/com/microService2/MicroService2Application.java
@@ -16,6 +16,7 @@ public class MicroService2Application {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroService2Application.class, args);
