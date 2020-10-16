b394ff13cf22f285c15102da31adbfcd4e3b4eba
sagardas1 <sagardas.195@gmail.com>
2020-10-16 15:02:46 +0530

    changes

--- Zull-api-gateway/src/main/java/com/apigateway/ZullApiGatewayApplication.java
+++ Zull-api-gateway/src/main/java/com/apigateway/ZullApiGatewayApplication.java
@@ -5,13 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZullApiGatewayApplication {
 
 	public static void main(String[] args) {
-		
+
 		SpringApplication.run(ZullApiGatewayApplication.class, args);
 	}
 
