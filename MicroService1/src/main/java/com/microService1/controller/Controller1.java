5b4acf062c33f89fe9fb8d9a350b37e5e19e26d0
sagardas1 <sagardas.195@gmail.com>
2020-10-17 20:50:10 +0530

    changes

--- MicroService1/src/main/java/com/microService1/controller/Controller1.java
+++ MicroService1/src/main/java/com/microService1/controller/Controller1.java
@@ -15,6 +15,7 @@ import com.microService1.service.Service1;
 public class Controller1 {
 	@Autowired
 	Service1 service1;
+	
 
 	@PostMapping(value = "/getName", headers = "Accept=application/json")
 	public String getName(@RequestBody UserDetails userDetails) {
